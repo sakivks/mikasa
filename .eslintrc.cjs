3436d7bd91be84ac3e32535412f54db5b800a7c5
vikas <vikas@stackbox.xyz>
2026-05-10 07:37:37 +0530

    chore: bootstrap TypeScript project with vitest, eslint, prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022, sourceType: 'module' },
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+  },
+  ignorePatterns: ['dist/', 'node_modules/', 'data-cache/', 'reports/', 'logs/'],
+};
---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['src/**/*.test.ts', 'test/**/*.test.ts'],
+    testTimeout: 15_000,
+    hookTimeout: 15_000,
+    pool: 'threads',
+  },
+});
